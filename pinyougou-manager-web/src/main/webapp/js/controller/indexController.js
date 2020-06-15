dcd3eb2dd169ac1422754fb3302bcad61539a0c6
jiahuigithub <jiahuigithu>
2020-06-15 22:03:00 +0800

    添加前端web

---
+++ pinyougou-manager-web/src/main/webapp/js/controller/indexController.js
@@ -0,0 +1,12 @@
+app.controller('indexController',function($scope,loginService){
+	
+	//显示当前用户名
+	$scope.showLoginName=function(){
+		loginService.loginName().success(
+			function(response){
+				$scope.loginName=response.loginName;
+			}
+		);
+	}
+	
+});
