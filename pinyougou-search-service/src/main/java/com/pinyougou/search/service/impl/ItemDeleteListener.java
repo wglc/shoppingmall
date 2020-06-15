d8220310a4a675e9026cc8b8bd70c4f211ef1aeb
wglc <wglc>
2020-06-15 22:43:03 +0800

    添加搜索

---
+++ pinyougou-search-service/src/main/java/com/pinyougou/search/service/impl/ItemDeleteListener.java
@@ -0,0 +1,41 @@
+package com.pinyougou.search.service.impl;
+
+import java.util.Arrays;
+
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.ObjectMessage;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.pinyougou.search.service.ItemSearchService;
+
+/**
+ * 监听：用于删除索引库中记录
+ * 
+ * @author Administrator
+ *
+ */
+
+@Component
+public class ItemDeleteListener implements MessageListener {
+
+	@Autowired
+	private ItemSearchService itemSearchService;
+
+	@Override
+	public void onMessage(Message message) {
+		ObjectMessage objectMessage = (ObjectMessage) message;
+		try {
+
+			Long[] goodsIds = (Long[]) objectMessage.getObject();
+			System.out.println("ItemDeleteListener监听接收到消息..." + goodsIds);
+			itemSearchService.deleteByGoodsIds(Arrays.asList(goodsIds));
+			System.out.println("成功删除索引库中的记录");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
